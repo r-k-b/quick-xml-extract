0104590058145c5fe1ef7668d3e0dd44e66331c0
rtzarius <rtzarius@gmail.com>
2014-06-19 22:48:53 +1000

    Set of images to grab is clean

--- img_grabber/img_grabber.py
+++ img_grabber/img_grabber.py
@@ -3,9 +3,15 @@ __author__ = 'VadShaytReth'
 
 import Queue
 import csv
+import os
+from pprint import pprint as pp
 from threading import Thread
 
+q = Queue.Queue(maxsize=0)
 csv_source = '../temp.csv'
+url_prefix_to_strip = '/images/imp'
+url_prefix_to_add = 'http://eangulus.com/bwidubbo/files'
+image_drop_folder = '../data/images'
 
 
 def do_work(item):
@@ -16,7 +22,8 @@ def source():
     """
     Fetch all the poplet URLs from the CSV file.
 
-    @:return array of strings
+    :rtype : iterable
+    :returns array of strings
 
     """
     with open(csv_source, 'rb') as csvfile:
@@ -34,21 +41,40 @@ def worker():
         do_work(item)
         q.task_done()
 
-q = Queue.Queue(maxsize=0)
+
+def transform_url(url=''):
+    de_prefixed = url.split(url_prefix_to_strip)[1]
+    re_prefixed = url_prefix_to_add + de_prefixed
+    return re_prefixed
 
 
-def grab(urls_to_check=None):
-    if not urls_to_check: urls_to_check = []
+def grab(urls_to_grab=None):
+    """
+    Download images from a defined location.
+
+    :param urls_to_grab: (array|set) of string
+    """
     for i in range(3):
         t = Thread(target=worker)
         t.daemon = True
         t.start()
 
-    for item in urls_to_check:
-        q.put(item)
+    for item in urls_to_grab:
+        q.put( transform_url(item))
 
     q.join()       # block until all tasks are done
 
+
 if __name__ == '__main__':
+    # Get URLs to work on
+    set_of_urls = set()
     for image_url in source():
-        print image_url
+        set_of_urls.add( transform_url(image_url))
+
+    pp(set_of_urls)
+
+    # TODO: Drop any we already have the file for
+    # for image_url in set_of_urls:
+
+
+    # Fetch & save the rest
